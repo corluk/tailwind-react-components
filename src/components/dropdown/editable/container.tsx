f7a3c169a42c20eadd96742ea1e5ae33c1a56781
osman corluk <corluk@hotmail.com>
2023-02-21 21:34:48 +0300

    d

---
+++ src/components/dropdown/editable/container.tsx
@@ -0,0 +1,24 @@
+import React , {useState}from "react"
+import tw from "tailwind-styled-components"
+interface DropdownEditableProps {
+
+    bgColor : string ,
+    f : 
+    className: string 
+}
+export default (props:DropdownEditableProps)=>{
+
+    const placeCss = [""]
+    const [value, setValue] = useState("") 
+    tw.div`
+      relative
+      bg-textBG  
+      text-textFG 
+      
+    `
+
+    return  <div className="">
+        <div
+    </div>
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   important: '#root',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palette : {
+          "001": "#000000",
+          "002": "#FFFFFF", 
+          "01" : "#000000" , 
+          "02" : "yellow" , 
+          "03" : "red" , 
+          "04" : "blue" ,
+          "05" : "green"
+        }
+      }
+       
+      }
+     
   },
   corePlugins: {
     // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
